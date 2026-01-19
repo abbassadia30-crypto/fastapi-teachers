a80dba63909a5984bc0b1646c5d9de1f338d6804
Ali hassan <ali0824728@gmail.com>
2026-01-19 19:43:29 +0500

    as

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,22 @@ import os
 from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 from backend.routers import auth, dashboard, institution
 from backend.database import engine
 import resend
 from . import models
 
-models.Base.metadata.drop_all(bind=engine)
-models.Base.metadata.create_all(bind=engine)
-
+def reset_database(engine):
+    with engine.connect() as conn:
+        # This tells PostgreSQL to ignore foreign key constraints during the drop
+        conn.execute(text("DROP SCHEMA public CASCADE;"))
+        conn.execute(text("CREATE SCHEMA public;"))
+        conn.commit()
+    
+    # Now recreate everything from your models
+    models.Base.metadata.create_all(bind=engine)
+reset_database()
 router = APIRouter()
 
 resend.api_key = os.getenv("RESEND_API_KEY", "your_key_here")
