8783df69333829c2f0a27f8747df37db791e558a
Ali hassan <ali0824728@gmail.com>
2026-01-19 19:50:52 +0500

    as

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ def reset_database(engine):
     
     # Now recreate everything from your models
     models.Base.metadata.create_all(bind=engine)
-reset_database()
+reset_database(engine)
 router = APIRouter()
 
 resend.api_key = os.getenv("RESEND_API_KEY", "your_key_here")
