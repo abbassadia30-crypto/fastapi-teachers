f8e2b2c0f1222d2be5133119052de59aac610561
Ali hassan <ali0824728@gmail.com>
2026-01-25 19:29:14 +0500

    fixed

--- backend/main.py
+++ backend/main.py
@@ -6,21 +6,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from backend.database import engine, Base
 from backend import models
 from backend.routers import auth, dashboard, institution, ready, pay, profile
+import logging
+# 🏛️ PEER TIP: This stops passlib from crashing on bcrypt version checks
+logging.getLogger("passlib").setLevel(logging.ERROR)
 
-# 1. Physical Delete
-#db_file = "institution.db"
-#if os.path.exists(db_file):
- #   os.remove(db_file)
-  #  print("🏛️ Old DB removed.")
-
-# 2. Create Tables
-#print("🏛️ Initializing Institution Database...")
 models.Base.metadata.create_all(bind=engine)
-
-# 3. SAFETY SLEEP (The Fix)
-# Give the file system 2 seconds to finish writing the .db file
-# time.sleep(2)
-print("🏛️ Database Tables Verified and Ready.")
 
 app = FastAPI()
 # ... include routers ...
