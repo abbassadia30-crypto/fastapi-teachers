ce1d4da3b300936d7ba770ec5437e7c615a15d73
Ali hassan <ali0824728@gmail.com>
2026-01-31 10:58:43 +0500

    fixed

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,4 @@
-from admin.document import Base  , Syllabus
-from admin.dashboard import Student , Staff , Teacher
-from admin.institution import Institution , School , Academy , College , User
-from admin.profile import UserBio , Profile
+from backend.models.admin.document import Base  , Syllabus
+from backend.models.admin.dashboard import Student , Staff , Teacher
+from backend.models.admin.institution import Institution , School , Academy , College , User
+from backend.models.admin.profile import UserBio , Profile
