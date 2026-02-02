63d7df1aa52decdeb4c1291fdeca9f7bcf8c5f44
Ali hassan <ali0824728@gmail.com>
2026-02-02 18:07:10 +0500

    fixed

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,8 @@ from backend.routers import document
 from backend.routers import profile
 from backend.routers import ready
 
+Base.metadata.drop_all(bind=engine)
+
 Base.metadata.create_all(bind=engine)
 logging.getLogger("passlib").setLevel(logging.ERROR)
 os.environ["PASSLIB_BUILTIN_BCRYPT"] = "enabled"
@@ -36,9 +38,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/dev/reset-db")
-def reset_db_endpoint():
-    from backend.reset_db import reset_database
-    reset_database()
-    return {"status": "DB reset successful"}
 
--- backend/reset_db.py
+++
@@ -1,7 +0,0 @@
-from backend.database import Base, engine
-
-def reset_database():
-    print("🧪 RESETTING DATABASE")
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    print("✅ DATABASE RESET COMPLETE")
