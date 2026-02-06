42d149b76943bc25d53e113e97d95c6378939e9d
Ali hassan <ali0824728@gmail.com>
2026-02-06 11:27:21 +0500

    fixed

--- backend/models/admin/role.py
+++ backend/models/admin/role.py
@@ -5,7 +5,7 @@ from backend.models.base import Base
 class Admin(Base):
     __tablename__ = 'admin'
     id = Column(Integer, primary_key=True)
-    username = Column(String, ForeignKey('users.username'))
+    username = Column(String, ForeignKey('users.name'))
     # CORRECTED: Moved ondelete into the ForeignKey constructor
     email = Column(String, ForeignKey("users.email", ondelete='CASCADE'))
     user_id = Column(Integer, ForeignKey("users.id"))
@@ -18,7 +18,7 @@ class Admin(Base):
 class Teacher(Base):
     __tablename__ = 'teacher'
     id = Column(Integer, primary_key=True)
-    username = Column(String, ForeignKey('users.username'))
+    username = Column(String, ForeignKey('users.name'))
     email = Column(String, ForeignKey("users.email"))
     user_id = Column(Integer, ForeignKey("users.id"))
     institution_id = Column(Integer, ForeignKey("institutions.id"))
@@ -30,7 +30,7 @@ class Teacher(Base):
 class Student(Base):
     __tablename__ = 'student'
     id = Column(Integer, primary_key=True)
-    username = Column(String, ForeignKey('users.username'))
+    username = Column(String, ForeignKey('users.name'))
     email = Column(String, ForeignKey("users.email"))
     user_id = Column(Integer, ForeignKey("users.id"))
     institution_id = Column(Integer, ForeignKey("institutions.id"))
