56ecf319dd6f024216f74803d4a5315b21bbbcf8
abbassadia30-crypto <abbassadia30@gmail.com>
2026-02-23 18:46:58 +0500

    Fixed

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Column, String, Integer
+from database import Base
+
 
 Base = declarative_base()
 
@@ -7,3 +10,4 @@ class TimestampMixin:
     """Adds created_at and updated_at to every table automatically"""
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, onupdate=func.now())
+
